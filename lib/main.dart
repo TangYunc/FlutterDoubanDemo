67a2e0c520c4ca026cda3894c25ef1157d597e03
tangyunchuan <tangyunchuan@yuanxin-inc.com>
2020-08-05 17:17:33 +0800

    底部bar实现

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MyStackPage();
+    return MaterialApp(
+      title: "豆瓣app",
+      theme: ThemeData(
+          primaryColor: Colors.green,
+        highlightColor: Colors.transparent,
+        splashColor: Colors.transparent
+      ),
+      home: MyStackPage(),
+    );
   }
 }
 
@@ -19,17 +27,36 @@ class MyStackPage extends StatefulWidget {
 }
 
 class MyStackPageState extends State<MyStackPage> {
+
+  var _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(
-      title: "豆瓣app",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("TYC"),
-        ),
-        body: Text("hello world"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TYC"),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        unselectedFontSize: 14,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text("首页")
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.more),
+              title: Text("更多")
+          )
+        ],
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+      body: Text("hello world"),
     );
   }
 }
